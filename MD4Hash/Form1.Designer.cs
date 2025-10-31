3f9837a03f588fc9642acf77eec43b765066df13
frog24 <dlavsakogogovna228@gmail.com>
2025-10-31 23:53:51 +0400

    сам алгоритм + отчет + исправление первого отчета

---
+++ MD4Hash/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace MD4Hash
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            labelStudentInfo = new Label();
+            textBoxInfo = new TextBox();
+            buttonSelectFile = new Button();
+            textBoxFilePath = new TextBox();
+            buttonCalculate = new Button();
+            textBoxHashResult = new TextBox();
+            buttonSaveHash = new Button();
+            labelFilePath = new Label();
+            labelHashResult = new Label();
+            richTextBoxAlgorithm = new RichTextBox();
+            SuspendLayout();
+            // 
+            // labelStudentInfo
+            // 
+            labelStudentInfo.AutoSize = true;
+            labelStudentInfo.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            labelStudentInfo.Location = new Point(9, 9);
+            labelStudentInfo.Margin = new Padding(4, 0, 4, 0);
+            labelStudentInfo.Name = "labelStudentInfo";
+            labelStudentInfo.Size = new Size(338, 18);
+            labelStudentInfo.TabIndex = 0;
+            labelStudentInfo.Text = "Студент: Барсуков П.О., Группа: ПИбд-41";
+            // 
+            // textBoxInfo
+            // 
+            textBoxInfo.BackColor = SystemColors.Control;
+            textBoxInfo.BorderStyle = BorderStyle.None;
+            textBoxInfo.Location = new Point(9, 32);
+            textBoxInfo.Margin = new Padding(4, 5, 4, 5);
+            textBoxInfo.Multiline = true;
+            textBoxInfo.Name = "textBoxInfo";
+            textBoxInfo.ReadOnly = true;
+            textBoxInfo.Size = new Size(209, 29);
+            textBoxInfo.TabIndex = 1;
+            textBoxInfo.Text = "Вариант: Хэш-функция MD4";
+            // 
+            // buttonSelectFile
+            // 
+            buttonSelectFile.Location = new Point(560, 173);
+            buttonSelectFile.Margin = new Padding(4, 5, 4, 5);
+            buttonSelectFile.Name = "buttonSelectFile";
+            buttonSelectFile.Size = new Size(133, 35);
+            buttonSelectFile.TabIndex = 5;
+            buttonSelectFile.Text = "Выбрать файл";
+            buttonSelectFile.UseVisualStyleBackColor = true;
+            buttonSelectFile.Click += buttonSelectFile_Click;
+            // 
+            // textBoxFilePath
+            // 
+            textBoxFilePath.Location = new Point(13, 177);
+            textBoxFilePath.Margin = new Padding(4, 5, 4, 5);
+            textBoxFilePath.Name = "textBoxFilePath";
+            textBoxFilePath.ReadOnly = true;
+            textBoxFilePath.Size = new Size(532, 27);
+            textBoxFilePath.TabIndex = 4;
+            // 
+            // buttonCalculate
+            // 
+            buttonCalculate.Location = new Point(701, 173);
+            buttonCalculate.Margin = new Padding(4, 5, 4, 5);
+            buttonCalculate.Name = "buttonCalculate";
+            buttonCalculate.Size = new Size(133, 35);
+            buttonCalculate.TabIndex = 6;
+            buttonCalculate.Text = "Вычислить MD4";
+            buttonCalculate.UseVisualStyleBackColor = true;
+            buttonCalculate.Click += buttonCalculate_Click;
+            // 
+            // textBoxHashResult
+            // 
+            textBoxHashResult.Location = new Point(17, 245);
+            textBoxHashResult.Margin = new Padding(4, 5, 4, 5);
+            textBoxHashResult.Name = "textBoxHashResult";
+            textBoxHashResult.ReadOnly = true;
+            textBoxHashResult.Size = new Size(676, 27);
+            textBoxHashResult.TabIndex = 8;
+            // 
+            // buttonSaveHash
+            // 
+            buttonSaveHash.Location = new Point(701, 242);
+            buttonSaveHash.Margin = new Padding(4, 5, 4, 5);
+            buttonSaveHash.Name = "buttonSaveHash";
+            buttonSaveHash.Size = new Size(133, 35);
+            buttonSaveHash.TabIndex = 9;
+            buttonSaveHash.Text = "Сохранить";
+            buttonSaveHash.UseVisualStyleBackColor = true;
+            buttonSaveHash.Click += buttonSaveHash_Click;
+            // 
+            // labelFilePath
+            // 
+            labelFilePath.AutoSize = true;
+            labelFilePath.Location = new Point(9, 152);
+            labelFilePath.Margin = new Padding(4, 0, 4, 0);
+            labelFilePath.Name = "labelFilePath";
+            labelFilePath.Size = new Size(135, 20);
+            labelFilePath.TabIndex = 3;
+            labelFilePath.Text = "Выбранный файл:";
+            // 
+            // labelHashResult
+            // 
+            labelHashResult.AutoSize = true;
+            labelHashResult.Location = new Point(13, 220);
+            labelHashResult.Margin = new Padding(4, 0, 4, 0);
+            labelHashResult.Name = "labelHashResult";
+            labelHashResult.Size = new Size(76, 20);
+            labelHashResult.TabIndex = 7;
+            labelHashResult.Text = "MD4-хэш:";
+            // 
+            // richTextBoxAlgorithm
+            // 
+            richTextBoxAlgorithm.Enabled = false;
+            richTextBoxAlgorithm.Location = new Point(13, 55);
+            richTextBoxAlgorithm.Margin = new Padding(4, 5, 4, 5);
+            richTextBoxAlgorithm.Name = "richTextBoxAlgorithm";
+            richTextBoxAlgorithm.Size = new Size(806, 87);
+            richTextBoxAlgorithm.TabIndex = 2;
+            richTextBoxAlgorithm.Text = resources.GetString("richTextBoxAlgorithm.Text");
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(840, 302);
+            Controls.Add(buttonSaveHash);
+            Controls.Add(textBoxHashResult);
+            Controls.Add(labelHashResult);
+            Controls.Add(buttonCalculate);
+            Controls.Add(buttonSelectFile);
+            Controls.Add(textBoxFilePath);
+            Controls.Add(labelFilePath);
+            Controls.Add(richTextBoxAlgorithm);
+            Controls.Add(textBoxInfo);
+            Controls.Add(labelStudentInfo);
+            Margin = new Padding(4, 5, 4, 5);
+            Name = "Form1";
+            Text = "MD4 Hash Calculator";
+            ResumeLayout(false);
+            PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelStudentInfo;
+        private System.Windows.Forms.TextBox textBoxInfo;
+        private System.Windows.Forms.Button buttonSelectFile;
+        private System.Windows.Forms.TextBox textBoxFilePath;
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.TextBox textBoxHashResult;
+        private System.Windows.Forms.Button buttonSaveHash;
+        private System.Windows.Forms.Label labelFilePath;
+        private System.Windows.Forms.Label labelHashResult;
+        private System.Windows.Forms.RichTextBox richTextBoxAlgorithm;
+    }
+}
---
+++ MD4Hash/MD4.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MD4Hash
+{
+    public static class MD4
+    {
+        /// <summary>
+        /// размер блока 
+        /// </summary>
+        private const int BlockSize = 64;
+
+        public static byte[] ComputeHash(byte[] input)
+        {
+            // Дополнение
+            byte[] paddedInput = PadMessage(input);
+
+            // Инициализация MD-буфера
+            uint A = 0x67452301;
+            uint B = 0xefcdab89;
+            uint C = 0x98badcfe;
+            uint D = 0x10325476;
+
+            // Обработка по 512-битным блокам - 16 слов по 32 бита
+            for (int i = 0; i < paddedInput.Length; i += BlockSize)
+            {
+                uint[] chunk = new uint[16];
+
+                for (int j = 0; j < 16; j++)
+                {
+                    chunk[j] = BitConverter.ToUInt32(paddedInput, i + j * 4);
+                }
+
+                uint AA = A;
+                uint BB = B;
+                uint CC = C;
+                uint DD = D;
+
+                // Раунд 1: Использует F, 16 шагов
+                FF(ref A, B, C, D, chunk[0], 3); FF(ref D, A, B, C, chunk[1], 7); FF(ref C, D, A, B, chunk[2], 11); FF(ref B, C, D, A, chunk[3], 19);
+                FF(ref A, B, C, D, chunk[4], 3); FF(ref D, A, B, C, chunk[5], 7); FF(ref C, D, A, B, chunk[6], 11); FF(ref B, C, D, A, chunk[7], 19);
+                FF(ref A, B, C, D, chunk[8], 3); FF(ref D, A, B, C, chunk[9], 7); FF(ref C, D, A, B, chunk[10], 11); FF(ref B, C, D, A, chunk[11], 19);
+                FF(ref A, B, C, D, chunk[12], 3); FF(ref D, A, B, C, chunk[13], 7); FF(ref C, D, A, B, chunk[14], 11); FF(ref B, C, D, A, chunk[15], 19);
+
+                // Раунд 2: Использует G, 16 шагов
+                GG(ref A, B, C, D, chunk[0], 3); GG(ref D, A, B, C, chunk[4], 5); GG(ref C, D, A, B, chunk[8], 9); GG(ref B, C, D, A, chunk[12], 13);
+                GG(ref A, B, C, D, chunk[1], 3); GG(ref D, A, B, C, chunk[5], 5); GG(ref C, D, A, B, chunk[9], 9); GG(ref B, C, D, A, chunk[13], 13);
+                GG(ref A, B, C, D, chunk[2], 3); GG(ref D, A, B, C, chunk[6], 5); GG(ref C, D, A, B, chunk[10], 9); GG(ref B, C, D, A, chunk[14], 13);
+                GG(ref A, B, C, D, chunk[3], 3); GG(ref D, A, B, C, chunk[7], 5); GG(ref C, D, A, B, chunk[11], 9); GG(ref B, C, D, A, chunk[15], 13);
+
+                // Раунд 3: Использует H, 16 шагов
+                HH(ref A, B, C, D, chunk[0], 3); HH(ref D, A, B, C, chunk[8], 9); HH(ref C, D, A, B, chunk[4], 11); HH(ref B, C, D, A, chunk[12], 15);
+                HH(ref A, B, C, D, chunk[2], 3); HH(ref D, A, B, C, chunk[10], 9); HH(ref C, D, A, B, chunk[6], 11); HH(ref B, C, D, A, chunk[14], 15);
+                HH(ref A, B, C, D, chunk[1], 3); HH(ref D, A, B, C, chunk[9], 9); HH(ref C, D, A, B, chunk[5], 11); HH(ref B, C, D, A, chunk[13], 15);
+                HH(ref A, B, C, D, chunk[3], 3); HH(ref D, A, B, C, chunk[11], 9); HH(ref C, D, A, B, chunk[7], 11); HH(ref B, C, D, A, chunk[15], 15);
+
+                A += AA;
+                B += BB;
+                C += CC;
+                D += DD;
+            }
+
+            // Формирование результата
+            byte[] output = new byte[16];
+            Array.Copy(BitConverter.GetBytes(A), 0, output, 0, 4);
+            Array.Copy(BitConverter.GetBytes(B), 0, output, 4, 4);
+            Array.Copy(BitConverter.GetBytes(C), 0, output, 8, 4);
+            Array.Copy(BitConverter.GetBytes(D), 0, output, 12, 4);
+
+            return output;
+        }
+
+        private static byte[] PadMessage(byte[] message)
+        {
+            int originalLength = message.Length;
+            int originalBitLength = originalLength * 8;
+
+            // Рассчет длины дополнения (padding)
+            // Нужно, чтобы (originalLength + 1 + paddingLength + 8) % 64 == 0
+            int paddingLength = (56 - (originalLength + 1) % 64 + 64) % 64;
+
+            // Создание нового массива с дополнением и длиной
+            byte[] padded = new byte[originalLength + 1 + paddingLength + 8];
+
+            // Копирование исходного сообщения
+            Array.Copy(message, 0, padded, 0, originalLength);
+
+            // Добавить байт 0x80 (представляет бит '1' и 7 нулей). Остальные байты после 0x80 и до длины уже равны 0 по умолчанию
+            padded[originalLength] = 0x80;
+
+            // 6. Добавление длины исходного сообщения в битах
+            Array.Copy(BitConverter.GetBytes((uint)originalBitLength), 0, padded, padded.Length - 8, 4);
+            Array.Copy(BitConverter.GetBytes((uint)0), 0, padded, padded.Length - 4, 4);
+
+            return padded;
+        }
+
+        // Лямбда выражения для логических операций
+        private static uint F(uint x, uint y, uint z) => (x & y) | (~x & z);
+        private static uint G(uint x, uint y, uint z) => (x & y) | (x & z) | (y & z);
+        private static uint H(uint x, uint y, uint z) => x ^ y ^ z;
+
+        /// <summary>
+        /// Выполняет один шаг первого раунда алгоритма MD4.
+        /// Применяет вспомогательную функцию F к трем регистрам (b, c, d), складывает результат
+        /// с текущим значением регистра a, добавляет слово из текущего 512-битного блока (x),
+        /// выполняет циклический сдвиг влево на заданное количество битов (s) и записывает результат обратно в регистр a.
+        /// </summary>
+        /// <param name="a">Ссылка на первый регистр (A, D, C или B в зависимости от шага).</param>
+        /// <param name="b">Второй регистр (входное значение).</param>
+        /// <param name="c">Третий регистр (входное значение).</param>
+        /// <param name="d">Четвёртый регистр (входное значение).</param>
+        /// <param name="x">Слово из 32-битного массива текущего 512-битного блока данных.</param>
+        /// <param name="s">Количество битов для циклического сдвига влево.</param>
+        private static void FF(ref uint a, uint b, uint c, uint d, uint x, byte s)
+        {
+            a += F(b, c, d) + x;
+            a = RotateLeft(a, s);
+        }
+
+        /// <summary>
+        /// Выполняет один шаг второго раунда алгоритма MD4.
+        /// Применяет вспомогательную функцию G к трем регистрам (b, c, d), складывает результат
+        /// с текущим значением регистра a, добавляет слово из текущего 512-битного блока (x),
+        /// прибавляет константу 0x5a827999, выполняет циклический сдвиг влево на заданное количество битов (s)
+        /// и записывает результат обратно в регистр a.
+        /// </summary>
+        /// <param name="a">Ссылка на первый регистр (A, D, C или B в зависимости от шага).</param>
+        /// <param name="b">Второй регистр (входное значение).</param>
+        /// <param name="c">Третий регистр (входное значение).</param>
+        /// <param name="d">Четвёртый регистр (входное значение).</param>
+        /// <param name="x">Слово из 32-битного массива текущего 512-битного блока данных.</param>
+        /// <param name="s">Количество битов для циклического сдвига влево.</param>
+        private static void GG(ref uint a, uint b, uint c, uint d, uint x, byte s)
+        {
+            a += G(b, c, d) + x + 0x5a827999;
+            a = RotateLeft(a, s);
+        }
+
+        /// <summary>
+        /// Выполняет один шаг третьего раунда алгоритма MD4.
+        /// Применяет вспомогательную функцию H к трем регистрам (b, c, d), складывает результат
+        /// с текущим значением регистра a, добавляет слово из текущего 512-битного блока (x),
+        /// прибавляет константу 0x6ed9eba1, выполняет циклический сдвиг влево на заданное количество битов (s)
+        /// и записывает результат обратно в регистр a.
+        /// </summary>
+        /// <param name="a">Ссылка на первый регистр (A, D, C или B в зависимости от шага).</param>
+        /// <param name="b">Второй регистр (входное значение).</param>
+        /// <param name="c">Третий регистр (входное значение).</param>
+        /// <param name="d">Четвёртый регистр (входное значение).</param>
+        /// <param name="x">Слово из 32-битного массива текущего 512-битного блока данных.</param>
+        /// <param name="s">Количество битов для циклического сдвига влево.</param>
+        private static void HH(ref uint a, uint b, uint c, uint d, uint x, byte s)
+        {
+            a += H(b, c, d) + x + 0x6ed9eba1;
+            a = RotateLeft(a, s);
+        }
+
+        /// <summary>
+        /// Выполняет циклический сдвиг 32-битного значения влево на заданное количество битов.
+        /// Биты, которые "выходят" слева, возвращаются в младшие разряды справа.
+        /// </summary>
+        /// <param name="value">32-битное значение для сдвига.</param>
+        /// <param name="count">Количество позиций для сдвига влево. Значение по модулю 32.</param>
+        /// <returns>Результат циклического сдвига влево.</returns>
+        private static uint RotateLeft(uint value, int count)
+        {
+            return (value << count) | (value >> (32 - count));
+        }
+    }
+}
